f3e299bbfc445cf78182085838ed46fe9c412702
suhailmajeed-pixel <suhailmajeed@gmail.com>
2026-01-09 18:03:50 +0530

    Add files via upload

---
+++ operator_precidence.py
@@ -0,0 +1,14 @@
+v = 4
+w = 7
+x = 3
+y = 6
+z = 0
+z = (v+w) * x/y ;
+print("value of (v+w) * x/y is:", z)
+
+name = "alax"
+age = 0
+if name == "alax" or name == "jhon" and age >= 2 :
+    print("hello! welcome.")
+else:
+    print("goodbye!!")
